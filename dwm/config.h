4e673765bbd33c1b13824c5045d9db5159613958
Thorvicnet <thorvicnet@gmail.com>
2025-10-12 22:35:33 +0200

    added slock and updated some things

--- dwm/config.h
+++ dwm/config.h
@@ -53,8 +53,8 @@ static const Rule rules[] = {
      *	WM_NAME(STRING) = title
      */
     /* class      instance    title       tags mask     isfloating   monitor */
-    {"Gimp", NULL, NULL, 0, 1, -1},
-    {"brave", NULL, NULL, 1 << 8, 0, -1},
+    {"Brave-browser", NULL, NULL, 1 << 1, 0, -1},
+    {"vesktop", NULL, NULL, 1 << 8, 0, -1},
 };
 
 /* layout(s) */
