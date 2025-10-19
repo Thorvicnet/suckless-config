e42b4459b631a8a1fb9ac9f8a953ec636e0e7ef5
Thorvicnet <thorvicnet@gmail.com>
2025-10-19 16:58:14 +0200

    added waylock and xdg-desktop-portal

--- start.sh
+++ start.sh
@@ -2,17 +2,18 @@
 
 export GTK_THEME=Adwaita-dark
 export XDG_SESSION_TYPE=wayland
-export XDG_CURRENT_DESKTOP=wlroots
+export XDG_CURRENT_DESKTOP=dwl
 export MOZ_ENABLE_WAYLAND=1
 export QT_QPA_PLATFORM=wayland
 export OZONE_PLATFORM=wayland
 
 "$HOME/.config/suckless/status-daemon/status_daemon" &
 exec dbus-run-session dwl -s '
-  dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+  dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE
   wbg "$HOME/Pictures/background/wave2.png" &
   mako &
   kdeconnectd &
   wl-paste --watch cliphist store &
   dwlb &
+  xdg-desktop-portal &
 '
